72df320b976efabd1325d9c052808538ddb78128
Richard Paget <richardpaget@gmail.com>
2022-08-24 13:45:41 +0100

    Tidy up

--- mpi-env-setup.sh
+++ mpi-env-setup.sh
@@ -7,8 +7,6 @@ echo "****************************************"
 echo "Setting up SSH configuration"
 echo "****************************************"
 
-#mkdir ~/.ssh
-#chmod 700 ~/.ssh 
 gsutil cp gs://$myBucket/id_rsa ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
@@ -17,24 +15,12 @@ echo "Host *" > ~/.ssh/config
 echo " StrictHostKeyChecking no" >> ~/.ssh/config
 chmod 644 ~/.ssh/config
 
-# Required to run IntelMPI
-source /opt/intel/psxe_runtime/linux/bin/psxevars.sh
-
-
-
 echo "****************************************"
 echo "Complete"
 echo "****************************************"
-#echo .
-#echo .
 echo "Please log into mpi-instance-02 before running below commands to allow SSH to function correctly (there is likely a better way to do this)"
-echo . 
-#echo "For OpenMPI please run:"
-#echo "/usr/lib64/openmpi/bin/mpirun -v -np 2 -hostfile /var/tmp/mpihosts /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency"
-#echo .
-#echo .
 echo "For Intel MPI please run:"
-#echo "source /opt/intel/psxe_runtime/linux/bin/psxevars.sh"
+echo "source /opt/intel/psxe_runtime/linux/bin/psxevars.sh"
 echo "then:"
 echo "mpirun -np 2 -ppn 1 -hosts mpi-instance-01,mpi-instance-02 IMB-MPI1 PingPong"
 echo "****************************************"
